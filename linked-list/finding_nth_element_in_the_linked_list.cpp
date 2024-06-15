d8e86f7bb2e36471c6d12b30d351a6895bc05c34
Taheershaik123 <sktaheer54@gmail.com>
2024-06-15 20:54:57 +0530

    new chalange

---
+++ linked-list/finding_nth_element_in_the_linked_list.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* next;
+    node(int val){
+        data = val;
+        next = NULL;
+
+    }
+};
+void insertion(node* &head,int val){
+    node* n = new node(val);
+    if(head == NULL){
+        head = n;
+        return;
+    }
+    node* temp = head;
+    while(temp->next != NULL ){
+        temp = temp->next;
+    }
+    temp->next = n;
+    n->next = NULL;
+}
+void display(node* head){
+    if(head == NULL){
+        cout<<"list is empty"<<endl;
+        return;
+    }
+    node* temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }cout<<endl;
+
+}
+void find_the_nth(node* &head,int val){
+    if(head == NULL){
+        cout<<"list is empty"<<endl;
+        return;
+    }
+    node* temp = head;
+    while(temp->data != val && temp != NULL){
+        temp = temp->next;
+    }
+    if(temp->data == val){
+        cout<<"the node is present in the linked list "<<endl;
+    }
+}
+int main(){
+    node* head = NULL;
+    insertion(head,1);
+    insertion(head,2);
+    insertion(head,3);
+    insertion(head,4);
+    insertion(head,5);
+    insertion(head,6);
+    display(head);
+    find_the_nth(head,4);
+}
