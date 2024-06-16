8f4b17fb89914b7796058f1a7f684cc1d9d4f2f4
Taheershaik123 <sktaheer54@gmail.com>
2024-06-17 01:19:22 +0530

    searching in the circular linked list

---
+++ linked-list/serching_in_circular_linked_list.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* next;
+    node(int val){
+        data = val;
+        next = NULL;
+
+    }
+};
+void insertion(node* &head,int val){
+    node* n = new node(val);
+    if(head == NULL){
+        head = n;
+        n->next = head;
+        return;
+    }
+    node* temp = head;
+    while(temp->next != head){
+        temp =  temp->next;
+    }
+    temp->next = n ;
+    n->next = head;
+}
+void display(node*  head){
+    node* temp = head;
+    if(head == NULL){
+        cout<<"the list is empty"<<endl;
+        return;
+    }
+    while(temp->next != head){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }cout<<endl;
+}
+void find(node* &head,int val){
+    node* temp = head;
+    if(head == NULL){
+        cout<<"the list is empty"<<endl;
+        return;
+
+    }
+    while(temp->next->data!= val &&  temp->next!= head){
+        temp = temp->next;
+    }
+    cout<<temp->next->data<<"element is present in linked list"<<endl;
+
+}
+int main(){
+    node* head =NULL;
+    insertion(head,1);
+    insertion(head,2);
+    insertion(head,3);
+    insertion(head,4);
+    insertion(head,5);
+    insertion(head,6);
+    insertion(head,7);
+    insertion(head,8);
+    insertion(head,9);
+    display(head);
+    find(head,8);
+}
