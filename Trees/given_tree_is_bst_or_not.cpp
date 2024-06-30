d713baddc3f897a256ab7e84ac37753de2bab2fa
Taheershaik123 <sktaheer54@gmail.com>
2024-06-30 13:40:22 +0530

    given tree is bst or not

---
+++ Trees/given_tree_is_bst_or_not.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* next;
+    node(int val){
+        data = val;
+        next = NULL;
+    }
+};
