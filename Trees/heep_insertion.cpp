33ed070732d40be794d40d5fc86cf5cf6e162e33
Taheershaik123 <sktaheer54@gmail.com>
2024-07-19 21:17:35 +0530

    heep insertion

---
+++ Trees/heep_insertion.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+    node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
