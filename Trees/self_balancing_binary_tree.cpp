693f5bfcb396609b0915a5ea0fd1e9c946564ac9
Taheershaik123 <sktaheer54@gmail.com>
2024-07-21 16:44:14 +0530

    self balancing binary tree

--- Trees/self_balancing_binary_tree.cpp
+++ Trees/self_balancing_binary_tree.cpp
@@ -13,6 +13,12 @@ class node{
         right = NULL;
     }
 };
+int counting(node* root){
+    if(root==NULL){
+        return 0;
+    }
+    return 1+counting(root->left)+counting(root->right);
+}
 node* insertion(node* root,int val){
     if(root == NULL){
         return new node(val);
@@ -26,4 +32,38 @@ node* insertion(node* root,int val){
     else if(root->right == NULL){
         root->right = insertion(root->right,val);
     }
+    else {
+        if(counting(root->left) <= counting(root->right)){
+            root->left = insertion(root->left,val);
+        }
+        else{
+            root->right = insertion(root->right,val);
+        }
+
+    }
+    return root;
 }
+
+void preorder(node* root){
+    if(root == NULL){
+        return ;
+    }
+    cout<<root->data<<"->";
+    preorder(root->left);
+    preorder(root->right);
+}
+int main(){
+    node* root = NULL;
+    root=insertion(root,1000);
+    insertion(root,1);
+    insertion(root,22);
+    insertion(root,21);
+    insertion(root,23);
+    insertion(root,28);
+    insertion(root,25);
+    insertion(root,12);
+    insertion(root,222);
+    insertion(root,23);
+    preorder(root);cout<<endl;
+    
+    }
