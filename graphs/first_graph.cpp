0170ac294164a72bca15c6397f99cde36e9e2553
Taheershaik123 <sktaheer54@gmail.com>
2024-07-27 21:09:43 +0530

    graph started

---
+++ graphs/first_graph.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<list>
+#include<vector>
+using namespace std;
+class Graph{
+    int v;
+    vector<vector<int>> adj;
+    public:
+    Graph(int v);
+    void Addedge(int v,int w);
+    void printGraph();
+
+};
+Graph::Graph(int v){
+
+  this->v = v;
+  adj.resize(v,vector<int>(v,0));
+}
+void Graph::Addedge(int v,int w){
+    adj[w][v]=1;
+    adj[w][v]=1;
+}
+void Graph::printGraph(){
+    for(int i=0;i<v;++i){
+        for(int j = 0;j<v;j++){
+            cout<<adj[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    Graph g(5);
+    g.Addedge(0,1);
+    g.Addedge(0,4);
+    g.Addedge(1,2);
+    g.Addedge(1,3);
+    g.Addedge(1,4);
+    g.Addedge(2,3);
+    g.Addedge(3,4);
+    g.printGraph();
+
+}
