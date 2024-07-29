5b4455376068f36acd1ebf91daf9e5b73a143478
Taheershaik123 <sktaheer54@gmail.com>
2024-07-29 11:48:34 +0530

    new pattren

---
+++ cpp/pattern1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the value of n "<<endl;
+    cin>>n;
+    for(int i =1;i<=i++){
+        for(int j=0;j<i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
