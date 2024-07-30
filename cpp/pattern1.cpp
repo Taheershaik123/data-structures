de7776c103baf6874bb55a1c104717bc648c932a
Taheershaik123 <sktaheer54@gmail.com>
2024-07-31 00:09:59 +0530

    new pattern

--- cpp/pattern1.cpp
+++ cpp/pattern1.cpp
@@ -1,10 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
+// 0
+// 0 1
+// 0 1 2
+// 0 1 2 3
+// 0 1 2 3 4
     int n;
     cout<<"enter the value of n "<<endl;
     cin>>n;
-    for(int i =1;i<=i++){
+    for(int i =1;i<=n;i++){
         for(int j=0;j<i;j++){
             cout<<j<<" ";
         }
--- cpp/pattern3.cpp
+++ cpp/pattern3.cpp
@@ -1,20 +1,19 @@
 #include<iostream>
 using namespace std;
-// 1
-// 2 2
-// 3 3 3
-// 4 4 4 4
-// 5 5 5 5 5
+// *****
+// ****
+// ***
+// **
+// *
 int main(){
     int n;
     cout<<"enter the value of n"<<endl;
     cin>>n;
-    
-    for (int i=0;i<=n;i++){
-        for(int j=0;j<i;j++){
-            cout<<i<<" ";
+    for(int i=n;i>0;i--){
+        for(int j=0; j<i;j++){
+            cout<<"*";
         }
         cout<<endl;
+    
     }
-
 }
