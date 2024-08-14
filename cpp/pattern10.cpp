d891d2a36e25aa145603a0a2884657e8e256049a
Taheershaik123 <sktaheer54@gmail.com>
2024-08-14 22:35:59 +0530

    new challenge

--- cpp/pattern10.cpp
+++ cpp/pattern10.cpp
@@ -1,10 +1,38 @@
 #include<iostream>
 using namespace std;
+      
+//      *
+//     **
+//    ***
+//   ****
+//  *****
+// ******
+//  *****
+//   ****
+//    ***
+//     **
+//      *
 int main(){
-    int n=10;
-    for(int i=0;i<n;i++){
-        for(int j=i;j<n;j++){
-            cout<<"@";
+    int n=5;
+    for(int i=0;i<=n;i++){
+        for(int j=n;j>=0;j--){
+            if(j<i){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=n;j++){
+            if(j<i){
+                cout<<" ";
+            }
+            else{
+                cout<<"*";
+            }
         }
         cout<<endl;
     }
