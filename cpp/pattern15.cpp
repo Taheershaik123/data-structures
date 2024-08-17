11d732bbb35840ce17dc4ff62d3219ebf959ca3e
Taheershaik123 <sktaheer54@gmail.com>
2024-08-18 02:16:36 +0530

    new chalenge started

---
+++ cpp/pattern15.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n*2;j++){
+            if(j<=i){
+               cout<<"*"<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
