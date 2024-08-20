a1e7e8a69366ef0c72ca84972be4af67eaa6c1a6
Taheershaik123 <sktaheer54@gmail.com>
2024-08-20 19:20:02 +0530

    new challenge

--- cpp/pattern15.cpp
+++ cpp/pattern15.cpp
@@ -4,8 +4,11 @@ int main(){
     int n=5;
     for(int i=0;i<n;i++){
         for(int j=0;j<n*2;j++){
-            if(j<=i){
+            if(j<=i||j==(n*2)-1){
                cout<<"*"<<" ";
+            }
+            else {
+                cout<<"  ";
             }
         }
         cout<<endl;
