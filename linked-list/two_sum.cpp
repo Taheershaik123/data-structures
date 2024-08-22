1d9a2dfd5cf29dc47d758a332445465c0eb2dd8d
Taheershaik123 <sktaheer54@gmail.com>
2024-08-23 00:38:54 +0530

    new challenge

---
+++ linked-list/two_sum.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* next;
+    node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+void insertion(node* &head,int val){
+    node* n = new node(val);
+    if(head == NULL){
+        head = n;
+        return;
+    }
+    node* temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = n;
+    n->next =NULL;
+}
+void display(node* head){
+    node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }cout<<endl;
+}
+node* List(node* l1,node* l2){
+    node* Listnew = new node();
+    node* temp = Listnew;
+    while(l1!=NULL||l2!=NULL){
+        int carry =0;
+        int x = (l1!=NULL)?l1->data:0;
+        int y = (l2!=NULL)?l2->data:0;
+        int sum = carry+x+y;
+        carry = sum/10;
+        temp->next = new node(sum%10);
+        temp = temp->next;
+        if(l1!=NULL&&l2!=NULL){
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+        
+    }
+    return Listnew;
+}
